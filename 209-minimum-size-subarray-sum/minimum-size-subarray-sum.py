d316c85471220d66cae3ae7d222e3a6d1213a975
gab <156394839+gabtlv@users.noreply.github.com>
2026-08-11 11:43:29 -0400

    Time: 18 ms (45.11%) | Memory: 30.6 MB (41.50%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,10 +1,12 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         #sliding window approach
-        left=0
+        #nums = [2,3,1,2,4,3]
+        left = 0
+        min_length = 1000000
         current_sum = 0
-        min_length = 1000000 #we want to find the minimum length here. so make the initial value infinity.
 
+        #sliding window
         for right in range(len(nums)):
             current_sum += nums[right]
             while current_sum >= target:
