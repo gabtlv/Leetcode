3e8e8754b5f8d5564469248cba6b58907ac4180f
gab <156394839+gabtlv@users.noreply.github.com>
2026-08-11 19:14:10 -0400

    Time: 20 ms (32.20%) | Memory: 32.3 MB (50.05%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,10 +1,11 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        # if any value appears at least twice, return true
-        # make another array, sort it, then compare with original
-        sortedNums = sorted(nums)
-        for i in range(len(sortedNums) - 1):
-            if sortedNums[i] == sortedNums[i+1]:
-                return True
-        return False
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
         
+        seen = set()
+
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                return True
+            else:
+                seen.add(nums[i])
+        return False
