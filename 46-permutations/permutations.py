d6f1b6050d9c136a2d9c735d2895563a9987c987
gab <156394839+gabtlv@users.noreply.github.com>
2026-08-11 15:41:22 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (87.23%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # result of the list 
+        result = []
+        # 1 2 3
+        # 2 3
+
+        # base case if theres only 1 element
+        if len(nums) == 1:
+            return [nums.copy()]
+
+        for i in range(len(nums)):
+            
+            initial = nums.pop(0)
+            perms = self.permute(nums)
+
+            for perm in perms:
+                perm.append(initial)
+            result.extend(perms)
+            nums.append(initial)
+        return result
+        
