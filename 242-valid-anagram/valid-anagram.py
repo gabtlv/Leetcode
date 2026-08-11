3f4835a99e8e8d12c83a95f4a6ad84f3688f039b
gab <156394839+gabtlv@users.noreply.github.com>
2026-08-11 19:04:00 -0400

    Time: 19 ms (16.41%) | Memory: 20.3 MB (18.57%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,17 +1,8 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        # take both strings
-        # rearrange in alphabetical order, then compare each letter to see if 
-        # it is a valid anagram
-        # check if they are the same length first
-        
-        if len(s) != len(t):
-            return False
-        
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         sortedS = sorted(s)
         sortedT = sorted(t)
-
-        for i in range(len(s)):
-            if sortedS[i] != sortedT[i]:
-                return False
-        return True
+        if sortedS == sortedT:
+            return True
+        else:
+            return False
