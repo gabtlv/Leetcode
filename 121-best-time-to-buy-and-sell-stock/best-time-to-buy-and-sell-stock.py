7886a8855de73ee556c47b98c87977234fec9b14
gab <156394839+gabtlv@users.noreply.github.com>
2026-08-11 19:28:20 -0400

    Time: 15 ms (98.21%) | Memory: 28.7 MB (20.39%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,12 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        # iterate through the whole array and see whats the best day to buy and sell
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         
         minPrice = prices[0]
-        maxProfit = 0 
+        maxProfit = 0
+
         for price in prices:
-            if price - minPrice  > maxProfit:
+            if price - minPrice > maxProfit:
                 maxProfit = price - minPrice
-                print(maxProfit)
             elif price < minPrice:
                 minPrice = price
         return maxProfit
